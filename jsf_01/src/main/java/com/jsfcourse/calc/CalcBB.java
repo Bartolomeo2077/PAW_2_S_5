62fd6c8eb854d1f21ab9ba3d22869863be66f896
barte <barte@Bartolomeo>
2023-10-17 22:57:25 +0200

    Kalkulator odsetek

---
+++ jsf_01/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -0,0 +1,89 @@
+package com.jsfcourse.calc;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+@Named
+@RequestScoped
+//@SessionScoped
+public class CalcBB {
+	private String kwota;
+	private String okres;
+	private String procent;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOkres() {
+		return okres;
+	}
+
+	public void setOkres(String okres) {
+		this.okres = okres;
+	}
+	public String getProcent() {
+		return procent;
+	}
+
+	public void setProcent(String procent) {
+		this.procent = procent;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double okres = Double.parseDouble(this.okres);
+			double procent = Double.parseDouble(this.procent);
+
+			result = (kwota * (procent/100)) / (okres/12);
+
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return true;
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return false;
+		}
+	}
+
+	// Go to "showresult" if ok
+	public String calc() {
+		if (doTheMath()) {
+			return "showresult";
+		}
+		return null;
+	}
+
+	// Put result in messages on AJAX call
+	public String calc_AJAX() {
+		if (doTheMath()) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Odsetki: " + result, null));
+		}
+		return null;
+	}
+
+	public String info() {
+		return "info";
+	}
+}
