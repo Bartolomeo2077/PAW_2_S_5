d63971ed53436f3c702a9a3934f7f72cfa7597a1
barte <barte@Bartolomeo>
2023-10-22 23:31:37 +0200

    Zadanie na 24.10.2023 - Wykonane

---
+++ primefaces_02/src/main/java/com/jsfcourse/calc/CalcBB.java
@@ -0,0 +1,71 @@
+package com.jsfcourse.calc;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class CalcBB {
+	private String kwota;
+	private String okres;
+	private String procent;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getKwota() {
+		return kwota;
+	}
+
+	public void setKwota(String kwota) {
+		this.kwota = kwota;
+	}
+
+	public String getOkres() {
+		return okres;
+	}
+
+	public void setOkres(String okres) {
+		this.okres = okres;
+	}
+	public String getProcent() {
+		return procent;
+	}
+
+	public void setProcent(String procent) {
+		this.procent = procent;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	public boolean doTheMath() {
+		try {
+			double kwota = Double.parseDouble(this.kwota);
+			double okres = Double.parseDouble(this.okres);
+			double procent = Double.parseDouble(this.procent);
+
+			result = (kwota * (procent/100)) / (okres/12);
+			
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return "showresult"; 
+		} catch (Exception e) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return null; 
+		}
+				
+	}
+
+	public String info() {
+		return "info"; 
+	}
+}
